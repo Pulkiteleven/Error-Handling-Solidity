7e1ea3f425ef4932daad53bcdd00e82d91e8b47c
Pulkiteleven <92856883+Pulkiteleven@users.noreply.github.com>
2024-04-13 13:14:45 +0530

    First Commit

---
+++ ErrorHandling.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+
+contract Error {
+    function testRequire(uint _i) public pure {
+        require(_i > 10, "Input must be greater than 10");
+    }
+
+    function testRevert(uint _i) public pure {
+        if (_i <= 10) {
+            revert("Input must be greater than 10");
+        }
+    }
+
+    uint public num;
+
+    function testAssert() public view {
+        assert(num == 0);
+    }
+
+
+}
